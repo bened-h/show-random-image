eb628a27555c2178d791516b54ccb842693977a9
Benedikt <bened-h@hotmail.com>
2018-02-22 18:47:48 +0100

    added function to show prev. images

--- show-rand-img.py
+++ show-rand-img.py
@@ -3,6 +3,7 @@ from random import choice
 import pygame
 from time import sleep
 
+
 class Pic():
     """class to hold the image"""
     shown_pics = []
@@ -15,14 +16,17 @@ class Pic():
         self.ydim_original = 0
         self.shown_pics.append(self)
         
-    def fit_to_screen(self, screen_size_x, screen_size_y):
+    def fit_to_screen(self, screen_size, fill_screen = False):
+        screen_size_x, screen_size_y = screen_size
         if self.xdim > screen_size_x: self.surface = pygame.transform.scale(self.surface, (screen_size_x,int(screen_size_x/self.ratio)))
-        if self.ydim > screen_size_y: self.surface = pygame.transform.scale(self.surface, (int(screen_size_y*self.ratio), screen_size_y))
-        self.xdim_original=self.xdim
-        self.ydim_original=self.ydim
+        elif fill_screen: self.surface = pygame.transform.scale(self.surface, (screen_size_x,int(screen_size_x/self.ratio)))
+        if self.surface.get_size()[1] > screen_size_y: self.surface = pygame.transform.scale(self.surface, (int(screen_size_y*self.ratio), screen_size_y))
+        self.xdim_original, self.ydim_original=self.xdim, self.ydim
         self.xdim, self.ydim = self.surface.get_size()
         
-def new_random_pic(fileslist):
+def get_new_random_pic(fileslist, fill_screen = False):
+    
+    #get pic
     while True:
         randomfilepath = choice(fileslist)  #load random filepath as string
         try:
@@ -34,9 +38,9 @@ def new_random_pic(fileslist):
     pic.surface=surf
     pic.xdim, pic.ydim = pic.surface.get_size()
     pic.ratio = pic.xdim/pic.ydim
-                    
-    return pic
     
+    return pic
+
 def main():
     #--- load image-files
     start_path = input("path: ")
@@ -47,13 +51,19 @@ def main():
             fileslist.append(os.path.join(path,filename))   # all image-paths in fileslist
             
     #--- setup pygame
-    pygame.init() 
-    screen_size_x = 1280
-    screen_size_y = 780 # Set the width and height of the screen
-    screen = pygame.display.set_mode((screen_size_x, screen_size_y))
+    pygame.init()
+    screen_size_x = 1800
+    screen_size_y = 980
+    randunten = 20
+    screen_size = screen_size_x , screen_size_y # Set the width and height of the screen
+    screen_size_pic = screen_size_x , screen_size_y -randunten
+    screen = pygame.display.set_mode(screen_size)
+    font = pygame.font.SysFont('Calibri', 12, True, False)
     pygame.display.set_caption("random image from " + start_path)
+    index = 0   # number of pic shown 
      
     clock = pygame.time.Clock() # Used to manage how fast the screen updates
+    fill_screen = True
 
     loop = True # Loop until the user clicks the close button.
     while loop: # -------- Main Program Loop -----------
@@ -63,15 +73,32 @@ def main():
                 loop = False
             elif event.type == pygame.KEYDOWN:  
                 if event.key == pygame.K_SPACE:     # if spacebar
+                    pic = get_new_random_pic(fileslist)
+                    pic.fit_to_screen(screen_size_pic, fill_screen) #resize image to fit screen                  
+                    #show pic
                     screen.fill((0,0,0))    #(0,0,0)==black     #clear screen
-                    pic = new_random_pic(fileslist)
-                    pic.fit_to_screen(screen_size_x,screen_size_y)
-                    #resize image to fit screen                  
-                    screen.blit(pic.surface, [0,0]) #image, position
-                    pygame.display.set_caption(pic.path)
+                    screen.blit(pic.surface, [screen_size_x/2-pic.xdim/2,0]) #image, position
+                    pygame.display.set_caption(pic.path) #update caption
+                    index = len(Pic.shown_pics)
+                elif event.key == pygame.K_d:  # delete image
+                    os.remove(pic.path)
+                    screen.fill((0,0,0))    #(0,0,0)==black     #clear screen
+                    Pic.shown_pics.remove(pic)
+                    print(pic.path + " deleted.")
+                    text = font.render(pic.path+ " deleted.", True, (255,255,255))
+                    screen.blit(text, [0, 0])
+                elif event.key == pygame.K_e or event.key == pygame.K_ESCAPE:
+                    loop = False
+                elif event.key == pygame.K_LEFT:
+                    if index!=0: index-=1
+                    else: continue
+                    pic = Pic.shown_pics[index]
+                    screen.fill((0,0,0))    #(0,0,0)==black     #clear screen
+                    screen.blit(pic.surface, [screen_size_x/2-pic.xdim/2,0]) #image, position
+                    pygame.display.set_caption(pic.path) #update caption#todo: show prev. pic
                     
         pygame.display.flip()  # update screen
-        clock.tick(60)      # --- Limit to frames per second
+        clock.tick(30)      # --- Limit to frames per second
          
     pygame.quit() # Close the window and quit.
 
